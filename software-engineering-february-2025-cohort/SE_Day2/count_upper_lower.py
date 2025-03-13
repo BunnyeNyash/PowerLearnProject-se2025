125d4403c7d1d2b9524a591c1b8e549ee22edb31
BunnyeNyash <125456472+BunnyeNyash@users.noreply.github.com>
2025-03-13 14:58:53 +0300

    counts lowercase, uppercase, and other characters in a sting from the user

---
+++ software-engineering-february-2025-cohort/SE_Day2/count_upper_lower.py
@@ -0,0 +1,28 @@
+# Write a Python function that takes a string as input and returns a dictionary containing the number of uppercase and lowercase characters in the string. Any characters that cannot be categorized as uppercase or lowercase (e.g., symbols) should be counted as "other".
+
+def count_uppercase_lowercase_other():
+    # Get string from user
+    string_1 = input("Enter your string: ")
+
+    # dictionary for storing the values for uppercase, lowercase, and other characters
+    dict = {
+        "uppercase" : 0,
+        "lowercase" : 0,
+        "other" : 0
+    }
+
+    string_list = list(string_1)
+
+    for char in string_list:
+        if char.isupper():
+            dict["uppercase"] += 1
+        elif char.islower():
+            dict["lowercase"] += 1
+        else:
+            dict["other"] += 1
+
+    return dict
+
+# invoke the function
+print(count_uppercase_lowercase_other())
+

8125ea1ff251477e5c742a2962e1b3d7438d640b
BunnyeNyash <125456472+BunnyeNyash@users.noreply.github.com>
2025-03-25 20:51:10 +0300

    Control flow and Functions weekly coding challenge

--- Python/Week_3_Control_Flow_and _Functions/weekly_coding_challenge/answers.py
+++ Python/Week_3_Control_Flow_and _Functions/weekly_coding_challenge/answers.py
@@ -25,3 +25,35 @@ def large_power(base, exponent):
 # test
 # print(large_power(10, 4))  # Output: True (10000 is greater than 5000)
 # print(large_power(2, 10))  # Output: False (1024 is NOT greater than 5000)
+
+
+
+
+def divisible_by_ten(num):
+    """
+    Determines if a number is divisible by 10.
+
+    Parameters:
+    num (int): The number to check.
+
+    Returns:
+    bool: True if num is divisible by 10, otherwise False.
+
+    Example:
+    >>> divisible_by_ten(50)
+    True
+    >>> divisible_by_ten(37)
+    False
+    >>> divisible_by_ten(0)
+    True
+    """
+
+    if num % 10 == 0:
+        return True
+    else:
+        return False
+    
+# print(divisible_by_ten(50))  # Output: True
+# print(divisible_by_ten(37))  # Output: False
+# print(divisible_by_ten(0)) # Output: True
+

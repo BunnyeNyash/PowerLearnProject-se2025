25c27ccaf54e14572ccd79b685c24b2867a4a0d3
BunnyeNyash <125456472+BunnyeNyash@users.noreply.github.com>
2025-04-16 01:25:37 +0300

    answers

---
+++ Database-Design-&-Programming-with-SQL/Week_1/assignment/answers.sql
@@ -0,0 +1,7 @@
+-- Write an SQL query to create a new database called **salesDB**.
+CREATE DATABASE salesDB;
+
+
+-- Write an SQL query to drop (delete) the database called **demo**.
+CREATE DATABASE demo;
+DROP DATABASE demo;

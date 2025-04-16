06b4486e10d9dd5d1facff4a4902b8cf85a23851
BunnyeNyash <125456472+BunnyeNyash@users.noreply.github.com>
2025-04-16 13:52:49 +0300

    answers

---
+++ Database-Design-&-Programming-with-SQL/Week_3/weekly_coding_challenge/student.sql
@@ -0,0 +1,14 @@
+-- student table with columns (name, age, gender)
+  CREATE TABLE student (
+    name VARCHAR(100) NOT NULL,
+    age INT NOT NULL,
+    gender VARCHAR(10) NOT NULL
+  );
+
+
+-- Insert at least 3 records into the student table. Each record should have a unique name, age, and gender.
+INSERT INTO student (name, age, gender) 
+VALUES
+  ('Alice Johnson', 20, 'Female'),
+  ('Brian Smith', 22, 'Male'),
+  ('Charlie Kim', 19, 'Non-binary');
